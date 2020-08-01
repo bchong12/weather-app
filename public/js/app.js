679ae4a64483eb2bdcf8e8ff4b88facfea73a564
Branden Chong <bchong12@g.ucla.edu>
2020-07-31 19:39:23 -0700

    Removed unnecesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('app js loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
